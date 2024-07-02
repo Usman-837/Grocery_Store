b15fe3d52faec8a04f114911ab5dcd4036859e78
Usman-837 <usmantufail837@gmail.com>
2024-07-02 15:57:14 +0500

    add menu bar in navbar component

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,6 +6,17 @@ const Navbar = () => {
         <header className="bg-white h-20 pt-6 flex justify-around">
             {/* logo */}
             <a href="" className=""><i className="fa-solid fa-basket-shopping text-green-700 text-xl"></i><pan className="text-purple-800 text-xl font-bold">Grocery</pan></a>
+
+            {/* Menu Bar */}
+            <nav className="text-gray-600">
+                <a href="" className="pl-8 hover:text-green-700">Home</a>
+                <a href="" className="pl-8 hover:text-green-700">Features</a>
+                <a href="" className="pl-8 hover:text-green-700">Products</a>
+                <a href="" className="pl-8 hover:text-green-700">Categories</a>
+                <a href="" className="pl-8 hover:text-green-700">Review</a>
+                <a href="" className="pl-8 hover:text-green-700">Blogs</a>
+            </nav>
+
         </header>
         </>
     );
