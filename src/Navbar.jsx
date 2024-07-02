418ea3dda86a74aff333670698ccd4434a835ca5
Usman-837 <usmantufail837@gmail.com>
2024-07-02 15:16:09 +0500

    create navbar component and logo with tailwindcss code

---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+        <>
+        <header className="bg-white h-20 pt-6 flex justify-around">
+            {/* logo */}
+            <a href="" className=""><i className="fa-solid fa-basket-shopping text-green-700 text-xl"></i><pan className="text-purple-800 text-xl font-bold">Grocery</pan></a>
+        </header>
+        </>
+    );
+};
+
+export default Navbar;
