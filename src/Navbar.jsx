a4ee7ae1bffa2d48888be8b9f4c94ec50a197808
Usman-837 <usmantufail837@gmail.com>
2024-07-02 17:40:05 +0500

    add search , shoping and user icons in navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
     return(
         <>
-        <header className="bg-white h-20 pt-6 flex justify-around">
+        <header className="bg-white h-24 pt-8 flex justify-around">
             {/* logo */}
             <a href="" className=""><i className="fa-solid fa-basket-shopping text-green-700 text-xl"></i><pan className="text-purple-800 text-xl font-bold">Grocery</pan></a>
 
@@ -17,6 +17,12 @@ const Navbar = () => {
                 <a href="" className="pl-8 hover:text-green-700">Blogs</a>
             </nav>
 
+            {/* icons */}
+            <div className="flex"> 
+                <div className="bg-gray-200 h-9 w-10 mr-2 boder rounded text-center pt-1.5 cursor-pointer"><i class="fa-solid fa-magnifying-glass"></i></div>
+                <div className="bg-gray-200 h-9 w-10 mr-2 boder rounded text-center pt-1.5 cursor-pointer"><i class="fa-solid fa-cart-shopping"></i></div>
+                <div className="bg-gray-200 h-9 w-10 boder rounded text-center pt-1.5 cursor-pointer"><i class="fa-solid fa-user"></i></div>
+            </div>
         </header>
         </>
     );
