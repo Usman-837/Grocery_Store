a870d9839b9235b8d70b9886f3950241bf19a557
Usman-837 <usmantufail837@gmail.com>
2024-07-03 11:59:19 +0500

    add default tailwind css code in index.css and in navbar component change logo color and add hover color in icons and search bar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <>
         <header className="bg-white h-24 pt-8 flex justify-around shadow-md shadow-gray-300">
             {/* logo */}
-            <a href="" className=""><i className="fa-solid fa-basket-shopping text-green-700 text-xl"></i><pan className="text-purple-800 text-xl font-bold">Grocery</pan></a>
+            <a href="" className=""><i className="fa-solid fa-basket-shopping text-[--green] text-xl"></i><pan className="text-purple-800 text-xl font-bold">Grocery</pan></a>
 
             {/* Menu Bar */}
             <nav className="text-gray-700">
@@ -19,15 +19,15 @@ const Navbar = () => {
 
             {/* icons */}
             <div className="flex"> 
-                <div className="search bg-gray-200 h-9 w-10 mr-2 boder rounded text-center pt-1.5 cursor-pointer"><i class="fa-solid fa-magnifying-glass"></i></div>
-                <div className="bg-gray-200 h-9 w-10 mr-2 boder rounded text-center pt-1.5 cursor-pointer"><i class="fa-solid fa-cart-shopping"></i></div>
-                <div className="bg-gray-200 h-9 w-10 boder rounded text-center pt-1.5 cursor-pointer"><i class="fa-solid fa-user"></i></div>
+                <div className="search bg-gray-200 h-9 w-10 mr-2 boder rounded text-center pt-1.5 cursor-pointer hover:bg-[--green] hover:text-white"><i class="fa-solid fa-magnifying-glass"></i></div>
+                <div className="bg-gray-200 h-9 w-10 mr-2 boder rounded text-center pt-1.5 cursor-pointer hover:bg-[--green] hover:text-white"><i class="fa-solid fa-cart-shopping"></i></div>
+                <div className="bg-gray-200 h-9 w-10 boder rounded text-center pt-1.5 cursor-pointer hover:bg-[--green] hover:text-white"><i class="fa-solid fa-user"></i></div>
             </div>
 
             {/* Seach Bar */}
             <form className="search-form absolute top-28 bg-white p-3 border rounded-lg w-96 text-gray-400 flex justify-between shadow-md shadow-gray-500 right-20">
                 <input type="search" name="" id="search-box" placeholder="Search here ....." className="normal-case"/>
-                <label for="search-box"><i className="fa-solid fa-magnifying-glass hover:text-green-600 font-bold text-lg"></i></label>
+                <label for="search-box"><i className="fa-solid fa-magnifying-glass hover:text-[--green] font-bold text-lg"></i></label>
             </form>
         </header>
         </>
