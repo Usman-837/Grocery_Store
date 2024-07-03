0b17735c93f6a56e76378aee97c23825aa3589ab
Usman-837 <usmantufail837@gmail.com>
2024-07-03 15:44:09 +0500

    regenerate toggle for search bar using usestate hook

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-import "./script.js";
+import React, { useState } from "react";
 
 const Navbar = () => {
+   
+    const [toggle , setToggle] = React.useState(false);
 
     return(
         <>
@@ -11,26 +12,29 @@ const Navbar = () => {
 
             {/* Menu Bar */}
             <nav className="text-gray-700">
-                <a href="" className="pl-8 hover:text-green-600">Home</a>
-                <a href="" className="pl-8 hover:text-green-600">Features</a>
-                <a href="" className="pl-8 hover:text-green-600">Products</a>
-                <a href="" className="pl-8 hover:text-green-600">Categories</a>
-                <a href="" className="pl-8 hover:text-green-600">Review</a>
-                <a href="" className="pl-8 hover:text-green-600">Blogs</a>
+                <a href="#" className="pl-8 hover:text-green-600">Home</a>
+                <a href="#" className="pl-8 hover:text-green-600">Features</a>
+                <a href="#" className="pl-8 hover:text-green-600">Products</a>
+                <a href="#" className="pl-8 hover:text-green-600">Categories</a>
+                <a href="#" className="pl-8 hover:text-green-600">Review</a>
+                <a href="#" className="pl-8 hover:text-green-600">Blogs</a>
             </nav>
 
             {/* icons */}
             <div className="flex"> 
-                <div className="search bg-gray-200 h-9 w-10 mr-2 boder rounded text-center pt-1.5 cursor-pointer hover:bg-[--green] hover:text-white"><i class="fa-solid fa-magnifying-glass"></i></div>
+                <div className="bg-gray-200 h-9 w-10 mr-2 boder rounded text-center pt-1.5 cursor-pointer hover:bg-[--green] hover:text-white" onClick={()=> setToggle(!toggle)}><i class="fa-solid fa-magnifying-glass"></i></div>
                 <div className="bg-gray-200 h-9 w-10 mr-2 boder rounded text-center pt-1.5 cursor-pointer hover:bg-[--green] hover:text-white"><i class="fa-solid fa-cart-shopping"></i></div>
                 <div className="bg-gray-200 h-9 w-10 boder rounded text-center pt-1.5 cursor-pointer hover:bg-[--green] hover:text-white"><i class="fa-solid fa-user"></i></div>
             </div>
 
             {/* Seach Bar */}
-            <form className="search-form absolute top-28 bg-white p-3 border rounded-lg w-96 text-gray-400 flex justify-between shadow-md shadow-gray-500 right-[-100%]">
-                <input type="search" name="" id="search-box" placeholder="Search here ....." className="normal-case"/>
+            {
+                toggle ? <form className="absolute top-28 bg-white p-3 border rounded-lg w-96 text-gray-400 flex justify-between shadow-md shadow-gray-500 right-20">
+                <input type="search" id="search-box" placeholder="Search here ....." className="normal-case"/>
                 <label for="search-box"><i className="fa-solid fa-magnifying-glass hover:text-[--green] font-bold text-lg"></i></label>
-            </form>
+                </form> : null
+            }
+                 
         </header>
         </>
     );
--- src/script.js
+++
@@ -1,4 +0,0 @@
-let searchForm = document.querySelector(".search-form");
-    document.querySelector(".search").onclick = () => {
-        searchForm.classList.toggle("active");
-    }
