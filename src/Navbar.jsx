3748a572e47adb4203c6b9a678d565e52c9827db
Usman-837 <usmantufail837@gmail.com>
2024-07-04 12:31:16 +0500

    add login page with toggle functionality and reset height of shopping cart

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
    
     const [search , setSearch] = React.useState(false);
     const [shopcart , setShopcart] = React.useState(false);
+    const [login , setLogin] = React.useState(false);
 
     return(
         <>
@@ -28,7 +29,7 @@ const Navbar = () => {
             <div className="flex"> 
                 <div className="bg-gray-200 h-9 w-10 mr-2 boder rounded text-center pt-1.5 cursor-pointer hover:bg-[--green] hover:text-white" onClick={()=> setSearch(!search)}><i class="fa-solid fa-magnifying-glass"></i></div>
                 <div className="bg-gray-200 h-9 w-10 mr-2 boder rounded text-center pt-1.5 cursor-pointer hover:bg-[--green] hover:text-white" onClick={()=> setShopcart(!shopcart)}><i class="fa-solid fa-cart-shopping"></i></div>
-                <div className="bg-gray-200 h-9 w-10 boder rounded text-center pt-1.5 cursor-pointer hover:bg-[--green] hover:text-white"><i class="fa-solid fa-user"></i></div>
+                <div className="bg-gray-200 h-9 w-10 boder rounded text-center pt-1.5 cursor-pointer hover:bg-[--green] hover:text-white" onClick={()=> setLogin(!login)}><i class="fa-solid fa-user"></i></div>
             </div>
 
             {/* Seach Bar */}
@@ -38,10 +39,10 @@ const Navbar = () => {
                 <label for="search-box"><i className="fa-solid fa-magnifying-glass hover:text-[--green] font-bold text-lg"></i></label>
                 </form> : null
             }
-
+            
             {/* Shopping Cart */}
             {
-                shopcart ? <div className="absolute top-28 bg-white p-3 border rounded-lg w-80 h-3/4 shadow-md shadow-gray-500 right-20">
+                shopcart ? <div className="absolute top-28 bg-white p-3 border rounded-lg w-80 shadow-md shadow-gray-500 right-20">
                 <div className="flex justify-between mt-2 p-2">
                     <img src={watermelon} className="w-24"/>
                     <div className="mt-6  mr-8">
@@ -74,7 +75,23 @@ const Navbar = () => {
                     <button className="btn w-full ">Checkout</button>
                 </div>
             </div> : null 
-            }    
+            } 
+
+            {/* login Page    */}
+            {
+                login ?  <div className="absolute top-28 bg-white border rounded-lg w-[360px] shadow-md shadow-gray-500 right-20">
+                <div className="flex flex-col justify-center p-8">
+                    <h1 className="text-center font-bold text-[--black] text-2xl uppercase mb-4">login now</h1>
+                    <input type="email" placeholder="Enter your email" className="bg-gray-200 rounded-md p-2 text-[--light-color] normal-case mb-4"/>
+                    <input type="password" placeholder="Enter your password" className="bg-gray-200 rounded-md p-2 text-[--light-color] normal-case mb-4"/>
+                    <p className="text-[--light-color] text-center mb-2 text-[15px]">forget your password <a href="#" className="text-[--green] underline"> click here</a></p>
+                    <p className="text-[--light-color] text-center text-[15px]">don't have and account <a href="#" className="text-[--green] underline"> create now</a></p>
+                    <div className="flex justify-center">
+                        <button className="btn w-52 text-[16px] capitalize">login now</button>
+                    </div>
+                </div>
+            </div> : null
+            }         
         </header>
         </>
     );
