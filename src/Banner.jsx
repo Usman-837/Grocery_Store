61bba235d3d89908d5ddb556555547ad80f794ad
Usman-837 <usmantufail837@gmail.com>
2024-07-13 13:08:20 +0500

    add some tailwindcss code with different custom break points to make responsive banner for small large mobile screen, tab, ipad, laptop, desktop and extra large screen

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Banner = () => {
     return(
         <>
-        <div className="bg-banner-img bg-cover bg-no-repeat h-[350px] flex justify-center"> 
-                <div className="mt-16 ml-20 w-[600px] sm:ml-4">
-                    <h3 className="font-bold text-3xl pb-3 pl-2 sm:text-xl sm:pl-0">fresh and <span className="text-[--green]">organic</span> products for you</h3>
-                    <p className="text-sm">Fresh And Organic Products Refer To Food Items That Are Produced And Prepared With Minimal Processing Without Use Of Synthetic Chemicals.</p>
-                    <a className="btn ml-40 sm:ml-20 mt-8">Shop Now</a>
+        <section id="home" className="bg-banner-img bg-cover bg-no-repeat h-[350px] flex justify-center"> 
+                <div className="w-[600px] small:w-[400px] text-center mt-16">
+                    <h3 className="font-bold text-3xl large:text-2xl small:text-lg pb-3 pl-2">fresh and <span className="text-[--green]">organic</span> products for you</h3>
+                    <p className="text-sm small:text-xs text-[--light-color] medium:text-black leading-6 mt-2">Fresh And Organic Products Refer To Food Items That Are Produced And Prepared With Minimal Processing Without Use Of Synthetic Chemicals.</p>
+                    <a className="btn mt-8 medium:pl-6 medium:pr-6 medium:text-sm small:text-white small:border-white">Shop Now</a>
                 </div>
-        </div>
+        </section>
         </>
     );
 };
